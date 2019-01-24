b56735b9377c50948eaa2fbbc7f0f710541d21f9
Stephen Clarkson <clarksones@gmail.com>
2019-01-23 18:07:38 -0800

    chore(TypeScript): Initialize TypeScript support

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,38 @@
 <template>
   <v-app>
-    <app-navigation-menu />
+    <v-toolbar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        flat
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-toolbar>
+
     <v-content>
-      <app-main-container />
+      <HelloWorld/>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import AppMainContainer from './components/AppMainContainer'
-import AppNavigationMenu from './components/AppNavigationMenu'
+import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
   components: {
-    AppMainContainer,
-    AppNavigationMenu
+    HelloWorld
+  },
+  data () {
+    return {
+      //
+    }
   }
 }
 </script>
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import './plugins/vuetify'
+import './plugins/vuetify'
+import './plugins/vuetify'
 import App from './App.vue'
 
 Vue.config.productionTip = false
