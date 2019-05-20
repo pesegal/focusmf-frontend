24f2865d51173d8897d5035999053ea2f0fe51d3
Peter <pesegal@gmail.com>
2019-05-20 16:58:02 -0700

    add vuex to project

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import '@/plugins/vuetify'
 import '@/plugins/vue-apollo'
+import '@/plugins/vuex'
 import App from '@/App.vue'
 import router from '@/router'
 import { createProvider } from './plugins/vue-apollo'
@@ -9,6 +10,7 @@ Vue.config.productionTip = false
 
 new Vue({
   router,
+  store, //TODO(peter): figure out how to get the proper type annotations for this object
   apolloProvider: createProvider(),
   render: h => h(App)
 }).$mount('#app')
