c4251442127893f7bcf71e16bc239b1a6e57fde9
Stephen Clarkson <clarksones@gmail.com>
2019-05-30 21:39:17 -0700

    test: Add initial login test

---
+++ cypress/integration/login.spec.js
@@ -0,0 +1,12 @@
+describe('Sign up process', () => {
+
+  beforeEach(() => {
+    cy.visit('http://localhost:8080/login')
+  })
+
+  describe('Username field', () => {
+    it('Given I am a user, when I enter an invalid email, then I cannot continue and I am notified', () => {
+
+    })
+  })
+})
