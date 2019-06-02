fdf0bf7956178389c5c2f40a4b3f80876e67a60d
Peter <pesegal@gmail.com>
2019-06-02 11:00:58 -0700

    wip: TaskList component

--- src/views/TasksView.vue
+++ src/views/TasksView.vue
@@ -1,7 +1,23 @@
 <template>
   <div class="TasksView">
     <v-container>
-      <h1>Tasks</h1>
+      <TaskList/>
     </v-container>
   </div>
 </template>
+
+<script lang="ts">
+import Vue from 'vue'
+import TaskList from '../components/TaskList.vue'
+
+export default {
+  components: {
+    TaskList
+  },
+  data () {
+    return {
+      greeting: "hello"
+    }
+  }
+}
+</script>
