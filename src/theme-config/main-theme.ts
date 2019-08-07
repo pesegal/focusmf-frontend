ba9b8f8be54dbf429085c12cca92fd7b16a458a7
Stephen Clarkson <clarksones@gmail.com>
2019-08-06 22:23:28 -0700

    feat: add theme definition

--- src/theme-config/main-theme.ts
+++ src/theme-config/main-theme.ts
@@ -1,3 +1,13 @@
 export default {
-  iconfont: 'md'
+  iconfont: 'md',
+  theme: {
+    themes: {
+      light: {
+        primary: '#d22121',
+        secondary: '#d22121',
+        accent: '#8c9eff',
+        error: '#b71c1c',
+      },
+    },
+  }
 }
