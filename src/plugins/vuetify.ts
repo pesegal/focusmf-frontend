d2e7285cfbd959f657225151600bc3893d5fdd37
Stephen Clarkson <clarksones@gmail.com>
2019-09-25 22:45:16 -0700

    feat: use vuetify 2.x

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,7 +1,10 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify'
-import 'vuetify/src/stylus/app.styl'
-import 'vuetify/dist/vuetify.min.css'
-import theme from '../theme-config/main-theme'
+import Vuetify from 'vuetify/lib'
 
-Vue.use(Vuetify, theme)
+Vue.use(Vuetify)
+
+export default new Vuetify({
+  icons: {
+    iconfont: 'mdi',
+  },
+})
