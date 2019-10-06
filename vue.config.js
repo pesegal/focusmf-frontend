e0dea84a9c9ae262c723032010dbab6af392912f
Stephen Clarkson <clarksones@gmail.com>
2019-10-05 23:26:06 -0700

    fix: use relative path
    This is expected in dev server proxy and nginx reverse proxy in production

--- vue.config.js
+++ vue.config.js
@@ -4,5 +4,13 @@ module.exports = {
   chainWebpack: config => {
     config.resolve.alias
       .set('@', path.resolve(__dirname, 'src/'))
+  },
+
+  devServer: {
+    proxy: {
+      '^/graphql': {
+        target: 'http://localhost:3000'
+      }
+    }
   }
 }
