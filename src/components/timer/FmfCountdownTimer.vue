19baa3cecbe0baee3d12ae771d4fb12e9e84bfb2
Peter Segal <pesegal@gmail.com>
2019-10-13 15:22:29 -0700

    wip: time start stop timer

--- src/components/timer/FmfCountdownTimer.vue
+++ src/components/timer/FmfCountdownTimer.vue
@@ -1,29 +1,25 @@
 <template>
-    <div class="FmfCountdownTimer">
-        <div class="display-4">{{ formattedTime }}</div>
+  <div class="FmfCountdownTimer">
+    <div class="display-4">
+      {{ formattedTime }}
     </div>
+  </div>
 </template>
 
 <script>
-    // Duration object from moment.js and countdown
-    export default {
-        props: {
-            time: {
-                type: Object,
-                required: true
-            }
-        },
-        // When duration reachs 0 then emit a notice of completion
-        computed: {
-            minutes() { return this.time.minutes.toString() },
-            seconds() { return this.time.seconds.toString() },
-            formattedTime() {
-                return `${this.minutes.padStart(2, "0")}:${this.seconds.padStart(2, "0")}`
-            }
-        }
+export default {
+  props: {
+    time: {
+      type: Object,
+      required: true
     }
+  },
+  computed: {
+    minutes() { return this.time.minutes.toString() },
+    seconds() { return this.time.seconds.toString() },
+    formattedTime() {
+      return `${this.minutes.padStart(2, '0')}:${this.seconds.padStart(2, '0')}`
+    }
+  }
+}
 </script>
-
-<style lang="scss">
-
-</style>
--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -2,14 +2,15 @@
   <div class="TimerView">
     <v-container>
       <h1>Timer</h1>
-      <fmf-countdown-timer :time="timeDisplay"/>
+      <fmf-countdown-timer :time="timeDisplay" />
+      <v-btn @click="toggleTimer">{{ timerStatus }}</v-btn>
     </v-container>
   </div>
 </template>
 
 <script>
 import FmfCountdownTimer from '@/components/timer/FmfCountdownTimer.vue'
-import moment from "moment"
+import moment from 'moment'
 
 export default {
   components: {
@@ -18,17 +19,40 @@ export default {
 
   data() {
     return {
-      currentTime: moment.duration(25, 'seconds'),
-      timeDisplay: { minutes: "0", seconds: "0" }
+      currentTime: moment.duration(10, 'seconds'),
+      timeDisplay: { minutes: '0', seconds: '0' },
+      countdownId: null,
+      timerIsStarted: false
     }
   },
-  mounted() {
-    setInterval(() => {
-      this.timeDisplay.minutes = this.currentTime.minutes()
-      this.timeDisplay.seconds = this.currentTime.seconds()
-      this.currentTime.subtract(1, 'seconds')
 
-    }, 1000)
+  computed: {
+    timerStatus() { return this.timerIsStarted ? "Pause" : "Start" }
+  },
+
+  methods: {
+    toggleTimer() {
+      if(!this.timerIsStarted) {
+        this.startTimer()
+      } else {
+        this.timerIsStarted = false
+        clearInterval(this.countdownId)
+      }
+    },
+
+    startTimer() {
+      this.timerIsStarted = true
+      this.countdownId = setInterval(() => {
+        this.timeDisplay.minutes = this.currentTime.minutes()
+        this.timeDisplay.seconds = this.currentTime.seconds()
+        if (this.currentTime.asSeconds() === 0) {
+          this.timerIsStarted = false
+          return clearInterval(this.countdownId)
+        }
+        this.currentTime.subtract(1, 'seconds')
+      }, 1000)
+    }
+
   }
 }
 </script>
