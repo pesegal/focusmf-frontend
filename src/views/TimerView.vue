abcd112c0d5ce270c03166452c07d82ffc14eeb7
Peter Segal <pesegal@gmail.com>
2019-10-13 13:17:51 -0700

    wip: timer display countdown

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -2,6 +2,33 @@
   <div class="TimerView">
     <v-container>
       <h1>Timer</h1>
+      <fmf-countdown-timer :time="timeDisplay"/>
     </v-container>
   </div>
 </template>
+
+<script>
+import FmfCountdownTimer from '@/components/timer/FmfCountdownTimer.vue'
+import moment from "moment"
+
+export default {
+  components: {
+    FmfCountdownTimer
+  },
+
+  data() {
+    return {
+      currentTime: moment.duration(25, 'seconds'),
+      timeDisplay: { minutes: "0", seconds: "0" }
+    }
+  },
+  mounted() {
+    setInterval(() => {
+      this.timeDisplay.minutes = this.currentTime.minutes()
+      this.timeDisplay.seconds = this.currentTime.seconds()
+      this.currentTime.subtract(1, 'seconds')
+
+    }, 1000)
+  }
+}
+</script>
