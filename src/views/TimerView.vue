3d7eeec384576439f05e6dfa64b7f10101033cd4
Peter <pesegal@gmail.com>
2019-10-14 19:59:29 -0700

    wip: add selected timer options

--- src/views/TimerView.vue
+++ src/views/TimerView.vue
@@ -2,8 +2,22 @@
   <div class="TimerView">
     <v-container>
       <h1>Timer</h1>
+      <v-btn @click="setDuration(25)">
+        Pomo
+      </v-btn>
+      <v-btn @click="setDuration(5)">
+        Short Break
+      </v-btn>
+      <v-btn @click="setDuration(15)">
+        Long Break
+      </v-btn>
       <fmf-countdown-timer :time="timeDisplay" />
-      <v-btn @click="toggleTimer">{{ timerStatus }}</v-btn>
+      <v-btn @click="toggleTimer">
+        {{ timerStatus }}
+      </v-btn>
+      <v-btn @click="resetTimer">
+        Reset
+      </v-btn>
     </v-container>
   </div>
 </template>
@@ -19,7 +33,8 @@ export default {
 
   data() {
     return {
-      currentTime: moment.duration(10, 'seconds'),
+      selectedTime: 25,
+      currentDuration: null,
       timeDisplay: { minutes: '0', seconds: '0' },
       countdownId: null,
       timerIsStarted: false
@@ -27,7 +42,7 @@ export default {
   },
 
   computed: {
-    timerStatus() { return this.timerIsStarted ? "Pause" : "Start" }
+    timerStatus() { return this.timerIsStarted ? 'Pause' : 'Start' }
   },
 
   methods: {
@@ -40,19 +55,39 @@ export default {
       }
     },
 
+    resetTimer() {
+      if(this.timerIsStarted) {
+        this.timerIsStarted = false
+        clearInterval(this.countdownId)
+        this.setDuration(this.selectedTime)
+        this.updateTimeDisplay(this.currentDuration)
+      }
+    },
+
+    updateTimeDisplay(currentDuration) {
+      this.timeDisplay.minutes = currentDuration.minutes()
+      this.timeDisplay.seconds = currentDuration.seconds()
+    },
+
     startTimer() {
       this.timerIsStarted = true
       this.countdownId = setInterval(() => {
-        this.timeDisplay.minutes = this.currentTime.minutes()
-        this.timeDisplay.seconds = this.currentTime.seconds()
-        if (this.currentTime.asSeconds() === 0) {
+        this.updateTimeDisplay(this.currentDuration)
+        if (this.currentDuration.asSeconds() === 0) {
           this.timerIsStarted = false
           return clearInterval(this.countdownId)
         }
-        this.currentTime.subtract(1, 'seconds')
+        this.currentDuration.subtract(1, 'seconds')
       }, 1000)
-    }
+    },
 
+    setDuration(minutes) {
+      if (!this.timerIsStarted) {
+        this.selectedTime = minutes
+        this.currentDuration = moment.duration(minutes, 'm')
+        this.updateTimeDisplay(this.currentDuration)
+      }
+    }
   }
 }
 </script>
